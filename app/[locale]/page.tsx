4077326ab0a3444d9c8c92dbb686f2ebd53d7dac
Nick Vigier <nvigier46@gmail.com>
2024-10-24 13:19:16 +0100

    test using cache-control headers

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -1,6 +1,6 @@
 import { headers } from "next/headers";
 
-export const dynamic = "force-static";
+// export const dynamic = "force-static";
 
 /** Add your relevant code here for the issue to reproduce */
 export default function Home({ params: { locale } }) {
--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,19 @@
  */
 const nextConfig = {
   reactStrictMode: true,
+  headers: async () => {
+    return [
+      {
+        source: '/:locale*',
+        headers: [
+          {
+            key: 'Vercel-CDN-Cache-Control',
+            value: 'max-age=3600'
+          }
+        ],
+      },
+    ];
+  }
 };
 
 export default nextConfig;
